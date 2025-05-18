8f9df4f524880a1d6c97bcf27fdace8e6108cdcc
B1229021 <144312167+B1229021@users.noreply.github.com>
2025-05-18 18:34:10 +0800

    Add files via upload

---
+++ linebot2.0/static/js/login_s.js
@@ -0,0 +1,19 @@
+const urlParams = new URLSearchParams(window.location.search);
+const u_id = urlParams.get('u_id');
+
+// 如果 URL 有帶 line_id，則自動填入隱藏欄位
+// if (u_id) {
+//     console.log("取得的 u_id:", u_id);
+//     document.getElementById('u_id').value = u_id;
+// } 
+// else {
+//     alert("密碼錯誤，請重新登入");
+// }
+
+// // 表單提交監聽事件
+// document.getElementById('loginForm').addEventListener('submit', function (e) {
+//     if (!u_id) {
+//         e.preventDefault();
+//         alert("Line ID 遺失，無法提交資料");
+//     }
+// });
